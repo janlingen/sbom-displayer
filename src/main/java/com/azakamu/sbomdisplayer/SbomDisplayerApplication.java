167cb8fa9bc6c0d5d3fc222ceab9a7b2af8e2dda
janlingen <82512348+janlingen@users.noreply.github.com>
2022-12-15 23:19:34 +0100

    Initialize spring boot project

---
+++ src/main/java/com/azakamu/sbomdisplayer/SbomDisplayerApplication.java
@@ -0,0 +1,16 @@
+package com.azakamu.sbomdisplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author janlingen
+ */
+@SpringBootApplication
+public class SbomDisplayerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SbomDisplayerApplication.class, args);
+  }
+
+}
