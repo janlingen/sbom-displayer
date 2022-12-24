d6e5b352f55745a09342ed49751f891efbbfa996
janlingen <82512348+janlingen@users.noreply.github.com>
2022-12-24 14:59:12 +0100

    Add custom findAllByName method

--- src/main/java/com/azakamu/sbomdisplayer/adapters/database/dataaccess/DependencyDAO.java
+++ src/main/java/com/azakamu/sbomdisplayer/adapters/database/dataaccess/DependencyDAO.java
@@ -1,6 +1,8 @@
 package com.azakamu.sbomdisplayer.adapters.database.dataaccess;
 
+import com.azakamu.sbomdisplayer.adapters.database.datatransfer.DependencyDTO;
 import com.azakamu.sbomdisplayer.adapters.database.datatransfer.ProjectDTO;
+import java.util.List;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Repository;
  * @author janlingen
  */
 @Repository
-public interface DependencyDAO extends Neo4jRepository<ProjectDTO, Long> {
+public interface DependencyDAO extends Neo4jRepository<DependencyDTO, Long> {
 
+  List<DependencyDTO> findAllByName(String name);
 }
