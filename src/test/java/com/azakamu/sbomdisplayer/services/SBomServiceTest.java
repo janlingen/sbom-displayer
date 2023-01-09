305ebafbcc247c2457c3c5296ea96fd145fce392
janlingen <82512348+janlingen@users.noreply.github.com>
2023-01-09 21:19:16 +0100

    Add test for SBomService

---
+++ src/test/java/com/azakamu/sbomdisplayer/services/SBomServiceTest.java
@@ -0,0 +1,36 @@
+package com.azakamu.sbomdisplayer.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.azakamu.sbomdisplayer.application.services.DependencyService;
+import com.azakamu.sbomdisplayer.application.services.ProjectService;
+import com.azakamu.sbomdisplayer.application.services.SBomService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * @author janlingen
+ */
+@ExtendWith(MockitoExtension.class)
+public class SBomServiceTest {
+
+  @Mock
+  private DependencyService dependencyService;
+
+  @Mock
+  private ProjectService projectService;
+
+  @InjectMocks
+  private SBomService sbomService;
+
+  @Test
+  void testParseProjectNameGradle() {
+    String sbom = "{\"metadata\":{\"component\":{\"name\":\"test-project\"}}}";
+    String expected = "test-project";
+    String actual = sbomService.parseProjectNameGradle(sbom);
+    assertEquals(expected, actual);
+  }
+}
